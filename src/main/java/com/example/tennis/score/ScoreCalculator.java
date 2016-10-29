8e6ad41075730e1cba7d2cbb1748a6bb9b3bbf6b
endris <endris@endris-PC>
2016-10-29 11:55:49 +0300

    adding Pitest

--- src/main/java/com/example/tennis/score/ScoreCalculator.java
+++ src/main/java/com/example/tennis/score/ScoreCalculator.java
@@ -3,5 +3,8 @@ package com.example.tennis.score;
 public class ScoreCalculator {
 
     public ScoreCalculator(String[] scoringSequence) {
+        if (null != scoringSequence) {
+            int length = scoringSequence.length;
+        }
     }
 }
